2ff03a3995fe94128bfb8a0a9899964355693049
Kw98 <kos091954@gmail.com>
2024-04-11 22:00:17 +0900

    0411
    Player Move & Dash

--- Assets/2. Scripts/Player.cs
+++ Assets/2. Scripts/Player.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player")]
     [SerializeField] private float speed;
 
     private bool sprint;
+
+    private bool isDodge;
+    private bool onDodge;
+    private float dodgeCooltime;
+
     private Vector3 moveVec;
+    private Vector3 dodgeVec;
 
     private Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Dodge();
     }
 
     private void Move()
@@ -29,13 +42,47 @@ public class Player : MonoBehaviour
         float vMove = Input.GetAxisRaw("Vertical");
         sprint = Input.GetButton("Sprint");
 
+        if (isDodge)
+            moveVec = dodgeVec;
+
         moveVec = new Vector3(hMove, 0, vMove).normalized;
 
         transform.position += moveVec * speed * Time.deltaTime;
 
-        animator.SetBool("Walk-Forward", moveVec != Vector3.zero);
-        animator.SetBool("Sprint-Forward", sprint);
+        animator.SetBool("isWalk", moveVec != Vector3.zero);
+        animator.SetBool("isSprint", sprint);
 
         transform.LookAt(transform.position + moveVec);
+    }
+
+    private void Dodge()
+    {
+        if (Input.GetButtonDown("Dodge") && moveVec != Vector3.zero && !onDodge)
+        {
+            animator.SetTrigger("Forward-Dodge");
+            isDodge = true;
+            onDodge = true;
+
+            Invoke("NextDodge", 0.5f);//Dodge Cooltime
+        }
+
+        if (dodgeCooltime <= 0)
+        {
+            speed = 5;
+            if (isDodge)
+                dodgeCooltime = 0.25f;
+        }
+        else
+        {
+            dodgeCooltime -= Time.deltaTime;
+            speed = 15;
+        }
+
+        isDodge = false;
+    }
+
+    private void NextDodge()
+    {
+        onDodge = false;
     }
 }
