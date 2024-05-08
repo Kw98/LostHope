6c46413acdc3cc3c3d207bfe971b185e4e5d5a3c
Kw98 <147613533+Kw98@users.noreply.github.com>
2024-05-08 17:54:35 +0900

    0508

--- 권오석(클라)/Assets/2. Scripts/Player/Bullet.cs
+++ 권오석(클라)/Assets/2. Scripts/Player/Bullet.cs
@@ -6,6 +6,12 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Wall")
